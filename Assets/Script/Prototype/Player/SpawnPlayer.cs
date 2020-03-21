77bc64808705670518ac56da99ea01ed4d1a2e71
Thierry <thierry-lam@hotmail.fr>
2020-03-21 13:17:27 +0100

    Spawn Player + Modification Unlock Room

---
+++ Assets/Script/Prototype/Player/SpawnPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    public PlayerController player;
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, spawnPoint.position, spawnPoint.rotation);
+    }
+}
--- Assets/Script/Prototype/System/UnlockNextRoom.cs
+++ Assets/Script/Prototype/System/UnlockNextRoom.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class UnlockNextRoom : MonoBehaviour
 {
+    public Door jail;
     public Door room1;
     public Door room2;
     public Door room3;
     public Door room4;
+    public Transform startpoint;
+    public Transform returnPoint;
+    public PlayerController player;
     int roomPassed;
 
     private void Awake()
     {
         roomPassed = PlayerPrefs.GetInt("roomPassed");
+        jail.CloseDoor();
         room1.OpenDoor();
         room2.CloseDoor();
         room3.CloseDoor();
         room4.CloseDoor();
-
+        Instantiate(player, startpoint.position, startpoint.rotation);
     }
 
     void Start()
@@ -27,18 +32,24 @@ public class UnlockNextRoom : MonoBehaviour
         switch (roomPassed)
         {
             case 1:
+                Instantiate(player, returnPoint.position, returnPoint.rotation);
+                jail.OpenDoor();
                 room1.OpenDoor();
                 room2.OpenDoor();
                 room3.CloseDoor();
                 room4.CloseDoor();
                 break;
             case 2:
+                Instantiate(player, returnPoint.position, returnPoint.rotation);
+                jail.OpenDoor();
                 room1.OpenDoor();
                 room2.OpenDoor();
                 room3.OpenDoor();
                 room4.CloseDoor();
                 break;
             case 3:
+                Instantiate(player, returnPoint.position, returnPoint.rotation);
+                jail.OpenDoor();
                 room1.OpenDoor();
                 room2.OpenDoor();
                 room3.OpenDoor();
@@ -50,6 +61,7 @@ public class UnlockNextRoom : MonoBehaviour
     void ResetPlayerPrefs()
     {
         PlayerPrefs.DeleteAll();
+        jail.CloseDoor();
         room1.OpenDoor();
         room2.CloseDoor();
         room3.CloseDoor();
