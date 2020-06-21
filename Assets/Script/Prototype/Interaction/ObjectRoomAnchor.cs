c25dc163869f755482449536cf9343c19d2da240
JimZucci <jimzucci5@gmail.com>
2020-06-21 17:10:12 +0200

    Anchors 1

---
+++ Assets/Script/Prototype/Interaction/ObjectRoomAnchor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRoomAnchor : MonoBehaviour
+{
+    public BoxCollider anchorCollider;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            anchorCollider.enabled = false;
+        }
+        else
+        {
+            anchorCollider.enabled = true;
+        }
+    }
+}
