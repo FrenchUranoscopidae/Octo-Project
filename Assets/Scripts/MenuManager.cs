72205e30b0aebb7e8e7d61abb9ec6e22da9f6af4
JimZucci <jimzucci5@gmail.com>
2020-06-22 15:08:16 +0200

    Inté Menu Pause principal 1

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MenuPause.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuPause : MonoBehaviour
+{
+
+    public bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        } 
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false; 
+    }
+     public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        //Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+     public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
