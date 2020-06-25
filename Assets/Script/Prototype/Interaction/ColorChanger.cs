9ba175aec689bf30c87c40c37c012f89dfc7e7e6
JimZucci <jimzucci5@gmail.com>
2020-06-25 14:41:20 +0200

    Color Changer Thunder 1

--- Assets/Script/Prototype/Interaction/ColorChanger.cs
+++ Assets/Script/Prototype/Interaction/ColorChanger.cs
@@ -13,10 +13,21 @@ public class ColorChanger : MonoBehaviour
     public AudioClip colorSwapSound;
     public GameObject colorSwapVisualEffect;
 
+    [Header("VFX")]
+    //public GameObject colorSwapThunder1;
+    //public GameObject colorSwapThunder2;
+    public bool vfxActive = false;
+
+    public ParticleSystem Thunder1;
+    public ParticleSystem Thunder2;
+
     void Start()
     {
         colorMgr.Initialize(GetComponent<SkinnedMeshRenderer>());
         colorMgr.InitializeObjectRenderer(GetComponent<MeshRenderer>());
+
+        Thunder1.Stop();
+        Thunder2.Stop();
     }
 
     public void SwapColors(ColorManager playerColorMgr)
@@ -25,8 +36,14 @@ public class ColorChanger : MonoBehaviour
         colorMgr.SwapObjectColors(playerColorMgr);
         StartCoroutine(SwapColorsTimer()); // Start the canSwap timer
         AudioSource.PlayClipAtPoint(colorSwapSound, transform.position);
-        Instantiate(colorSwapVisualEffect, transform.position, transform.rotation);
+        //Instantiate(colorSwapVisualEffect, transform.position, transform.rotation);
         Destroy(GameObject.Find("ColorSwap(Clone)"), 2f);
+
+        //colorSwapThunder1.SetActive(true);
+        //colorSwapThunder2.SetActive(true);
+
+        Thunder1.Play();
+        Thunder2.Play();
 
         //DialogueTriggering
         if (!b_dialogueHappenned)
