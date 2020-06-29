de579e1656081d697e698612ad173ca350c5822f
ErwanMettouchi <36893161+ErwanMettouchi@users.noreply.github.com>
2020-06-29 21:06:05 +0200

    MenusUpdate
    Menu Principal + Menu Pause sur chaque scène avec navigation clavier

--- Assets/Script/Prototype/UI/MenuManager.cs
+++ Assets/Script/Prototype/UI/MenuManager.cs
@@ -6,14 +6,46 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    
-    public void Start()
+    public GameObject creditsMenu,menu;
+    public GameObject  creditsBackSelected, creditsClosed;
+
+    public void OpenCredits() 
     {
-        
+        creditsMenu.SetActive(true);
+        menu.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(creditsBackSelected);
+
     }
-    public void PlayGame()
+    public void CloseCredits()
+    {
+        creditsMenu.SetActive(false);
+        menu.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(creditsClosed);
+    }
+        public void PlayGame()
     {
         SceneManager.LoadScene(1);
+        Time.timeScale = 1;
+    }
+
+    public void CheckPoint1()
+    {
+        SceneManager.LoadScene(2);
+        Time.timeScale = 1;
+    }
+    public void CheckPoint2()
+    {
+        SceneManager.LoadScene(3);
+        Time.timeScale = 1;
+    }
+    public void CheckPoint3()
+    {
+        SceneManager.LoadScene(4);
+        Time.timeScale = 1;
     }
     public void QuitGame()
     {
