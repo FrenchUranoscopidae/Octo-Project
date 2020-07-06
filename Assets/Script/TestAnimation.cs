ab88ab424259e4a926c0531c62ea5017430f42b9
Meaux Saint Marc Philippine <philippinemsm@laposte.net>
2020-07-06 17:01:52 +0200

    Revert "animation"
    This reverts commit 6a80e4601a425674878e9d8790d07f3b0ea9e2a4.

--- Assets/Script/TestAnimation.cs
+++ Assets/Script/TestAnimation.cs
@@ -6,31 +6,25 @@ public class TestAnimation : MonoBehaviour
 {
     private float defaultZ;
     private float counter;
-    private float counter2;
-    public float initialOffsetX;
     public float initialOffsetZ;
     public float initialOffsetY;
-    public float rotationAmountX;
     public float rotationAmountZ;
     public float rotationAmountY;
     public GameObject rootBone;
     public int switchDirection;
-    public int switchDirection2;
-    private float xAngle;
     private float yAngle;
     private float zAngle;
     private Vector3 initialRotation = new Vector3();
-
+    
     void Start()
     {
         //Change object's initial rotation to have an equal animation on both sides.
-        rootBone.transform.Rotate(initialOffsetX, initialOffsetY, initialOffsetZ);
+        rootBone.transform.Rotate(0, initialOffsetY, initialOffsetZ);
         initialRotation.Set(rootBone.transform.eulerAngles.x, rootBone.transform.eulerAngles.y, rootBone.transform.eulerAngles.z);
         counter = 0;
         defaultZ = 0;
         yAngle = 0;
         zAngle = 0;
-        xAngle = 0;
     }
 
     void FixedUpdate()
@@ -45,24 +39,9 @@ public class TestAnimation : MonoBehaviour
                 rotationAmountY *= -1;
                 counter = 0;
             }
-
             yAngle += rotationAmountY;
             zAngle += rotationAmountZ;
             rootBone.transform.eulerAngles = initialRotation+new Vector3(0, transform.eulerAngles.y+yAngle+180.0f, zAngle);
-        }
-        else
-        {
-            rootBone.transform.localEulerAngles = new Vector3(0, 0, 0);
-            counter2 ++;
-
-            if (counter2 >= switchDirection2)
-            {
-                rotationAmountX *= -1;
-                counter2 = 0;
-            }
-
-            xAngle += rotationAmountX;
-            rootBone.transform.eulerAngles = new Vector3(initialOffsetX + xAngle, 0, 0);
         }
     }
 }
